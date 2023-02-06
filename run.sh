6858aec07456142d31b1854209d863a4148d2b78
Deep Lalwani <deepthesmartest@gmail.com>
2023-02-06 11:18:19 +0530

    Run all script

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
-g++ $1_*.cpp -lglut -lGL -lGLU -o main.run
-./main.run
-rm main.run
+if [ $1 = "all" ]; then
+	for i in {0..43}
+	do
+		g++ "$i"_*.cpp -lglut -lGL -lGLU -o main.run
+		echo "\nRunning Program $i"
+		./main.run
+		rm main.run
+	done
+else
+	g++ $1_*.cpp -lglut -lGL -lGLU -o main.run
+	./main.run
+	rm main.run
+fi
